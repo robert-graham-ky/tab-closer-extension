99dcdf47143a7d1e4c70a008e35119f66396f45c
RobDawg-sama <pickles40324@gmail.com>
2023-01-15 03:26:48 -0500

    updated function to handle the edge case where the current tab is not the first instance of that site, so the first tab will be closed

--- assets/popup.js
+++ assets/popup.js
@@ -1,15 +1,21 @@
-document.getElementById("close-duplicates-button").addEventListener("click", closeDuplicateTabs);
+document
+  .getElementById("close-duplicates-button")
+  .addEventListener("click", closeDuplicateTabs);
 
 function closeDuplicateTabs() {
-    chrome.tabs.query({currentWindow: true}, function(tabs) {
-        let uniqueTabs = {};
-        for (let tab of tabs) {
-            if(tab.active) continue;
-            if (uniqueTabs[tab.url]) {
-                chrome.tabs.remove(tab.id);
-            } else {
-                uniqueTabs[tab.url] = tab.id;
-            }
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    let uniqueTabs = {};
+    for (let tab of tabs) {
+      if (uniqueTabs[tab.url]) {
+        if (tab.active) {
+          chrome.tabs.remove(uniqueTabs[tab.url]);
+          uniqueTabs[tab.url] = tab.id;
+        } else {
+          chrome.tabs.remove(tab.id);
         }
-    });
+      } else {
+        uniqueTabs[tab.url] = tab.id;
+      }
+    }
+  });
 }
